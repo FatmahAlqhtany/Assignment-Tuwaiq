7c7d273f0246055c60ad555f8a66bf7a7ed532bd
unknown <Fatimah.Alqhtany@tuwaiq.edu.sa>
2021-05-23 15:27:26 +0300

    add mvc homewrok

---
+++ MVC APP/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication11.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+      public string Index()
+        {
+            return "Default action";
+        }
+        public string Welcome(string name)
+        {
+            return $"welcome {name}";
+        }
+
+    }
+}
